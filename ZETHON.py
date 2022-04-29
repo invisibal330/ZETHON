539db3ad706691464c168adaf85b855b0ea102da
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-04-29 16:08:18 +0100

    ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("ZETHON").main()
+       __import__("ZETHON").keyx()
    except Exception as a:
        exit(str(a))
