57f0ff40b7afab39f79b4f0e5f0d2de3e8bcd0cb
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 15:27:36 +0100

    N

--- ZETHON.py
+++ ZETHON.py
@@ -11,5 +11,6 @@ if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
 		__import__("ZETHON").login()
-	else:
 		__import__("ZETHON32").login()
+	else:
+		__import__("ZETHON64").login()
