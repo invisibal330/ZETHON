360a96491fbd778db86e7c4a503b8fde123f9340
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 14:11:08 +0100

    Update ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -5,7 +5,8 @@ comb =platform.architecture()[0]
 if __name__ == "__main__":
 	os.system("git pull")
 	if comb == "64bit":
-		__import__("ZETHON64").login()
+		from ZETHON64 import login
+                login()
 	elif comb == "32bit":
 		__import__("ZETHON32").login()
 	else:
