2b84b6ba78352cd199dfce82592f9ee920aec58c
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 13:50:00 +0100

    ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -5,9 +5,9 @@ comb =platform.architecture()[0]
 if __name__ == "__main__":
 	os.system("git pull")
 	if comb == "64bit":
-		__import__("ZETHON").login()
-	elif comb == "32bit":
 		__import__("ZETHON64").login()
+	elif comb == "32bit":
+		__import__("ZETHON32").login()
 	else:
 		print("UNKNOWN SYSTEM ")
 		exit()
