b273d117668182782e43f72b90f106d3002c19e8
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 15:29:57 +0100

    B

--- ZETHON.py
+++ ZETHON.py
@@ -10,7 +10,7 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("ZETHON").login()
 		__import__("ZETHON32").login()
-	else:
-		__import__("ZETHON64").login()
+		__import__("ZETHON").login()
+	print("UNKNOWN SYSTEM ")
+		exit()
