021fd0d9c894e0fe1c44d8e0be28114a960bfd9a
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-16 17:45:45 +0100

    Update ZETHON.py

--- ZETHON.py
+++ ZETHON.py
@@ -1,19 +1,18 @@
-import platform
 import os
 os.system('termux-setup-storage')
-#os.system('git pull')
-#try:os.system('mkdir /sdcard/Chi-DATA')
-#except:pass
-#try:os.system('mkdir /sdcard/Chi-DATA/OK')
-#except:pass
-#try:os.system('mkdir /sdcard/Chi-DATA/CP')
-#except:pass
+os.system('git pull')
+try:os.system('mkdir /sdcard/CHI-DATA')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/TAP-A2F')
+except:pass
 try:os.system('touch .prox.txt')
 except:pass
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-        __import__("ZETHON").login()
-elif 'aarch' in arc:
-        __import__("ZETHON").login()
-else:
-        exit(f' Unknow device machine {arc}')
+if __name__ == "__main__":
+        try:
+                __import__("ZETHON").login()
+        except Exception as e:
+                exit(str(e))
