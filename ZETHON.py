92740efff4025430938639e64c16f6ce1caa2bac
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-16 14:57:56 +0100

    .

--- ZETHON.py
+++ ZETHON.py
@@ -1,5 +1,16 @@
 P = '\x1b[1;97m'
-import os,requests
+import platform
+import os
+os.system('termux-setup-storage')
+#os.system('git pull')
+#try:os.system('mkdir /sdcard/PROHACK-DATA')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/OK')
+#except:pass
+#try:os.system('mkdir /sdcard/PROHACK-DATA/CP')
+#except:pass
+try:os.system('touch .prox.txt')
+except:pass
 xr = requests.get("http://ip-api.com/json/").json()
 try:
 	fc = xr["country"]
